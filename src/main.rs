cc7d5f88829479c1c34ca21c83fb4199b92b5751
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-06-07 15:06:15 +0200

    feat: implement window for boids simulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+use crate::boids::Boids;
+
+mod boid;
+mod boids;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Boids simulation".to_owned(),
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut boids = Boids::new(10);
+
+    loop {
+        clear_background(BLACK);
+
+        boids.update();
+        boids.draw();
+
+        next_frame().await
+    }
 }
