f7c94fea37973f7dc7fa794a471b86727bbdef56
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-06-08 23:34:49 +0200

    feat: add boids config

--- src/config/config.rs
+++ src/config/config.rs
@@ -5,21 +5,41 @@ use toml::from_str;
 
 #[derive(Deserialize)]
 pub struct Config {
+    pub window: WindowConfig,
+    pub boids: BoidsConfig,
+}
+
+#[derive(Deserialize)]
+pub struct WindowConfig {
     pub window_title: String,
     pub window_height: i32,
     pub window_width: i32,
+}
+
+#[derive(Deserialize)]
+pub struct BoidsConfig {
+    pub number: i32,
+    pub min_speed: f32,
+    pub max_speed: f32,
+    pub max_force: f32,
+    pub neighborhood_radius: f32,
+    pub separation_radius: f32,
+    pub separation_strength: f32,
+    pub alignment_strength: f32,
+    pub cohesion_strength: f32,
+    pub debug: bool,
 }
 
 impl Config {
     pub fn new(path: &str) -> Self {
         let config = match read_to_string(path) {
             Ok(content) => content,
-            Err(_) => return Config::default()
+            Err(_) => return Config::default(),
         };
 
         match from_str::<Config>(&config) {
             Ok(config) => config,
-            Err(_) => Config::default()
+            Err(_) => Config::default(),
         }
     }
 }
@@ -27,9 +47,23 @@ impl Config {
 impl Default for Config {
     fn default() -> Self {
         Config {
-            window_title: "Boids simulation".to_owned(),
-            window_height: 600,
-            window_width: 800
+            window: WindowConfig {
+                window_title: "Boids simulation".to_owned(),
+                window_height: 600,
+                window_width: 800,
+            },
+            boids: BoidsConfig {
+                number: 100,
+                min_speed: 2.0,
+                max_speed: 5.0,
+                max_force: 0.05,
+                neighborhood_radius: 60.0,
+                separation_radius: 40.0,
+                separation_strength: 3.0,
+                alignment_strength: 1.8,
+                cohesion_strength: 0.5,
+                debug: false,
+            },
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,16 @@ pub static CONFIG: Lazy<Config> = Lazy::new(|| Config::new("config.toml"));
 
 fn window_conf() -> Conf {
     Conf {
-        window_title: CONFIG.window_title.clone(),
-        window_height: CONFIG.window_height,
-        window_width: CONFIG.window_width,
+        window_title: CONFIG.window.window_title.clone(),
+        window_height: CONFIG.window.window_height,
+        window_width: CONFIG.window.window_width,
         ..Default::default()
     }
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut boids = Boids::new(10, 6.0);
+    let mut boids = Boids::new(CONFIG.boids.number, 6.0);
 
     loop {
         clear_background(BLACK);
